888bf2adb0c69468b098173f01494f145df3b25b
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2019-11-26 11:47:47 +0700

    update

---
+++ src/12-plotly-heatmap.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.offline as pyo
+
+df = pd.read_csv('../dataset/2010SantaBarbaraCA.csv')
+
+data = [go.Heatmap(
+    x=df['DAY'],
+    y=df['LST_TIME'],
+    z=df['T_HR_AVG'],
+    colorscale='Jet'
+)]
+
+layout = go.Layout(
+    title='Hourly Temperatures, June 1-7, 2010 in<br>\
+    Santa Barbara, CA USA'
+)
+
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig)
